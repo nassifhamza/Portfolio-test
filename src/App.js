afdb30411a06935c7857667bbdb095affca9bdcc
HAMZA NASSIF <158636275+nassifhamza@users.noreply.github.com>
2025-07-14 15:11:00 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,19 +19,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [load, upadateLoad] = useState(true);
-
+  
   useEffect(() => {
     const timer = setTimeout(() => {
       upadateLoad(false);
     }, 1200);
-
     return () => clearTimeout(timer);
   }, []);
-
+  
   return (
     <Router>
       <Preloader load={load} />
-      <div className="App" id={load ? "no-scroll" : "scroll"}>
+      <div 
+        className="App" 
+        id={load ? "no-scroll" : "scroll"}
+        data-testid="app"
+      >
         <Navbar />
         <ScrollToTop />
         <Routes>
